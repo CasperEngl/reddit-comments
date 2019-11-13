bae52ee27d5b5a43eb7870d7622e212ea44c7a6d
Casper <me@casperengelmann.com>
2019-11-13 08:36:30 +0100

    formatting + linting fixes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import * as path from 'path';
 
-import * as puppeteer from 'puppeteer';
-import * as commandLineArgs from 'command-line-args';
-import * as slug from 'slug';
-import * as downloadsFolder from 'downloads-folder';
 import { mkdirp } from 'fs-extra';
+import * as puppeteer from 'puppeteer';
+import slug from 'slug';
+import commandLineArgs from 'command-line-args';
+import downloadsFolder from 'downloads-folder';
 
 import { optionDefinitions } from './optionDefinitions';
 import { UrlArgumentMissing } from './UrlArgumentMissing';
@@ -17,7 +17,7 @@ const launch = async () => {
   }
 
   const browser = await puppeteer.launch({
-    headless: argv.head || true,
+    headless: argv.head ?? true,
   });
   const page = await browser.newPage();
 
@@ -36,13 +36,15 @@ const launch = async () => {
         || span.innerHTML.includes('edited')
         || span.innerHTML.includes('days ago')
         ))
-        .forEach((span: HTMLElement) => span.parentNode.removeChild(span));
+        .forEach(
+          (span: HTMLElement) => span.parentNode?.removeChild(span)
+        );
     }, 'body');
   }
 
   const titleElement = await page.$x('//h1[last()]');
 
-  const title = slug(await page.evaluate(element => element.textContent, titleElement.pop()), {
+  const title = slug(await page.evaluate((element) => element.textContent, (titleElement.pop() as puppeteer.ElementHandle<Element>)), {
     lower: true,
   });
 
@@ -50,13 +52,15 @@ const launch = async () => {
 
   const comments = await Promise.all(
     spans.map(
-      (span) => span.$x('../..').then((result) => result.pop()),
+      (span) => span.$x('../..').then(
+        (result) => result.pop()
+      ),
     ),
   );
 
   for (const comment of comments) { // eslint-disable-line
-    const classNames = await comment.getProperty('className');
-    const value = await classNames.jsonValue();
+    const classNames = await comment?.getProperty('className');
+    const value = await classNames?.jsonValue();
     const name = slug(value, {
       lower: true,
     });
@@ -67,7 +71,7 @@ const launch = async () => {
 
     await mkdirp(dest);
 
-    await comment.screenshot({
+    await comment?.screenshot({
       path: `${dest}/${name}.png`,
     });
   }
